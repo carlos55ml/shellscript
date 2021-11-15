b6ee3700191ccc1aac80a7f02664314e07a957de
carlos55ml <cmoranlozano55@gmail.com>
2021-11-15 17:41:07 +0100

    Modified script 2 and added folder ejercicios

--- script2.sh
+++ script2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [[ $# < 3 ]]; then
-	echo 'Se deben pasar 3 parametros en la ejecucion'
-	echo 'Ejemplo: ~# ./'$0' parametro1 parametro2 parametro3'
-	exit 0
+if [[ $# < 1 ]]; then
+	echo 'Se deben pasar parametros en la ejecucion'
+	echo 'Ejemplo: ~# ./'$0' parametro1 parametro2 parametro3...'
+	exit 1
 fi
 
-echo 'El primer parametro es: '$1
-echo 'El segundo parametro es: '$2
-echo 'El tercer parametro es: '$3 
-
+for i in `seq 1 1 $#`; do
+	echo "La variable $i es $1";
+	shift 1;
+done;
 exit 0
